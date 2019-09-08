fe6d824a8bf06da0ac41a5f2dc72fac45dde1f05
MannyBoy Flores <mlflores2@miners.utep.edu>
2019-09-08 13:14:14 -0600

    has command not found, can run ls

--- shell/myShell.py
+++ shell/myShell.py
@@ -1,8 +1,23 @@
 #! /usr/bin/env python3
 
-import os, sys, time, re
+import os, sys, re
 
-while(1):
+while (1):
     inp = input("$")
-    if(inp == "ls"):
-        print("files here")
+    child_pid = os.fork()
+    if child_pid == 0:
+        for dir in re.split(":", os.environ['PATH']): # try each directory in the path
+         program = "%s/%s" % (dir, inp)
+         try:
+             os.execve(program, [inp], os.environ) # try to exec program
+             os.wait()
+         except FileNotFoundError:             # ...expected
+             pass
+        os.write(1,(inp + ":command not found\n").encode())
+    elif child_pid < 0:
+        os.write(2, ("fork failed, returning %d\n" % rc).encode())
+        sys.exit(1)
+    else:
+        os.wait()
+    
+        
