5e4563976d39f3bca5c815b55f413571be8951ce
MannyBoy Flores <mlflores2@miners.utep.edu>
2019-09-08 16:45:30 -0600

    has exit command and ps1

--- shell/myShell.py
+++ shell/myShell.py
@@ -3,20 +3,26 @@
 import os, sys, re
 
 while (1):
-    inp = input("$")
+    inp = input(os.getcwd() + "$ ")
+
+    if inp == "exit":
+        sys.exit()
     child_pid = os.fork()
+
     if child_pid == 0:
         for dir in re.split(":", os.environ['PATH']): # try each directory in the path
          program = "%s/%s" % (dir, inp)
          try:
              os.execve(program, [inp], os.environ) # try to exec program
-             os.wait()
          except FileNotFoundError:             # ...expected
              pass
         os.write(1,(inp + ":command not found\n").encode())
+        sys.exit()
+
     elif child_pid < 0:
-        os.write(2, ("fork failed, returning %d\n" % rc).encode())
+        os.write(2, ("Program terminated with exit code %d.\n" % rc).encode())
         sys.exit(1)
+
     else:
         os.wait()
     
