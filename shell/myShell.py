1964bf6a40c8744c790c468bcf852ed7b7dcab02
MannyBoy Flores <mlflores2@miners.utep.edu>
2019-09-21 16:46:34 -0600

    changed redirect, pipe not working

--- shell/myShell.py
+++ shell/myShell.py
@@ -27,27 +27,56 @@ while (1):
         except FileNotFoundError:             # ...expected
             os.write(1,(args[1] + " :file not found\n").encode())
         continue
-    
+
     child_pid = os.fork()  
     if child_pid == 0:
-         if len(args) >= 2:
-            if args[1] == ">":
-                os.close(1)
-                fd = open( args[2],"w+")
-                sys.stdout = fd
-                os.set_inheritable(1, True)
-                exe(args[0])
-                os.close(fd)
-            if args[1] == "<":
-                os.close(0)
-                fd = open( args[2],"w+")
-                sys.stdout = fd
-                os.set_inheritable(0, True)
-                exe(args[0])
-                os.close(fd)
-            if args[1] == "|":
-                pass
-         exe(args[0])
+        if len(args) >= 2:
+            for arg in range(len(args)):
+                if arg == len(args):
+                    break
+                if args[arg] == ">":
+                    newCHPID = os.fork()
+                    if newCHPID == 0:
+                        os.close(1)
+                        fd = open( args[arg +1],"w+")
+                        sys.stdout = fd
+                        os.set_inheritable(1, True)
+                        exe(args[arg -1])
+                        os.close(fd)
+                    else:
+                        os.wait()
+                    continue
+                elif args[arg] == "<":
+                    newCHPID = os.fork()
+                    if newCHPID == 0:
+                        os.close(0)
+                        fd = open( args[arg - 1],"w+")
+                        sys.stdin = fd
+                        os.set_inheritable(0, True)
+                        exe(args[arg + 1])
+                        os.close(0)
+                    else:
+                        os.wait()
+                    continue
+                elif args[arg] == "|":
+                    (pr,pw) = os.pipe()
+                    for f in (pr, pw):       #setting and remeber ing pipe fd's
+                        os.set_inheritable(f, True) 
+                    newCHPID = os.fork()
+                    if newCHPID == 0:
+                        os.close(1)          #close stdout
+                        os.dup(pr)           # new stdout is pipe input
+                        exe(args[arg -1])
+                        os.close(pr)
+                    else:
+                        os.wait()
+                        os.close(0)          #close stdin
+                        os.dup(pw)           #new stdin is pipe out
+                        exe(args[arg + 1])
+                        os.close(pw)
+                    continue
+        else:               
+            exe(args[0])
             
 
     elif child_pid < 0:
